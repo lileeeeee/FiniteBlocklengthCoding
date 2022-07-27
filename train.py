1868406c974d9f1bdf86beea21232f4e0b9c6119
lizheng <li-zheng@sjtu.edu.cn>
2022-07-27 14:44:56 +0800

    gen_metric 1.1

--- train.py
+++ train.py
@@ -30,8 +30,8 @@ if gpus:
 def main(args):
     def get_data(n_train, n_dim):
         # 10d of Gaussian
-        x = BR_data.gen_nd_Gaussian(n_dim, n_train)
-        np.savetxt('./dataset_for_training/Gaussian_10d_mixture.dat', x)
+        # x = BR_data.gen_nd_Gaussian(n_dim, n_train)
+        # np.savetxt('./dataset_for_training/Gaussian_10d_mixture.dat', x)
         x = np.loadtxt('./dataset_for_training/Gaussian_10d_mixture.dat').astype(np.float32)[:n_train,:]
         return x
     print("------ Loading data ------")
@@ -106,8 +106,8 @@ def main(args):
         return -tf.reduce_mean(tf.math.log(real + 1e-10) + tf.math.log(1. - fake + 1e-10))
 
     def get_g_loss(fake):
-        return tf.reduce_mean(1. - fake)
-        # return -tf.reduce_mean(tf.math.log(fake + 1e-10))
+        # return tf.reduce_mean(1. - fake)
+        return -tf.reduce_mean(tf.math.log(fake + 1e-10))
 
     def inv_sphere_proj(raw_point, n_dim, n_train, radius):
         """inverse stereographic projection
@@ -138,18 +138,42 @@ def main(args):
     g_optim = tf.keras.optimizers.Adam(learning_rate=args.g_lr)
     d_optim = tf.keras.optimizers.Adam(learning_rate=args.d_lr)
 
-    # summary
-    summary_writer = tf.summary.create_file_writer(args.summary_dir)
+    def g_metric_base(c, n_dim):
+        r_sq = n_dim * (1 + c) * np.log(1 + c) / c
+        return tf.math.igamma(n_dim / 2, r_sq / 2) - tf.math.igamma(n_dim / 2, r_sq / (1 + c) / 2)
 
-    def getBinaryTensor(imgTensor, boundary=0.5):
-        one = tf.ones_like(imgTensor)
-        zero = tf.zeros_like(imgTensor)
-        return tf.where(imgTensor > boundary, one, zero)
+    def d_real_loss(real, fake):
+        return tf.reduce_mean(real) + tf.reduce_mean(1. - fake)
+
+    def g_metric(n_dim, c, n_sample=1000):
+        mean = np.zeros(n_dim)
+        var1 = np.identity(n_dim)
+        var2 = np.diag([1 + c] * n_dim)
+
+        np.random.seed(42)
+        ori = np.random.multivariate_normal(mean, var1, n_sample)
+        contrast = np.random.multivariate_normal(mean, var2, n_sample)
+
+
+        ori = kr_model(ori)[0]
+        ori = inv_sphere_proj(ori, n_dim, n_sample, args.radius) + BR_data.gen_nd_Gaussian(n_dim + 1, n_sample)
+
+        contrast = kr_model(contrast)[0]
+        contrast = inv_sphere_proj(contrast, n_dim, n_sample, args.radius) + BR_data.gen_nd_Gaussian(n_dim + 1, n_sample)
+
+        ori = Discriminator(ori, training=False)
+        contrast = Discriminator(contrast, training=False)
+
+        ori = tf.where(ori < 0.5, 0., 1.)
+        contrast = tf.where(contrast < 0.5, 0., 1.)
+
+        tf.print(g_metric_base(c, n_dim), d_real_loss(ori, contrast))
+        return
 
     # prepare one training iteration step
     def kr_train_step(inputs, kr_para, g_para, d_para):
         x_t = inputs
-        d_loss, g_loss = 0.0, 0.0
+        d_loss, g_loss = tf.constant(0.0), tf.constant(0.0)
         with tf.GradientTape() as kr_tape:
             """generate real data"""
             y_t = kr_model(x_t)[0]
@@ -169,128 +193,53 @@ def main(args):
                     sample_gaussian_vector = BR_data.gen_nd_Gaussian(args.n_dim + 1, fake_inputs.shape[0]) #generate stochastic noise
                     fake_inputs = fake_inputs + sample_gaussian_vector
 
+                    pre_d_loss = tf.constant(1000.)
+
                     for j in tf.range(args.d_epoch):
                         with tf.GradientTape() as d_tape:
                             """discriminator step"""
-                            tf.print("------dis_step", j + 1,"------")
+                            # tf.print("------dis_step", j + 1,"------")
 
                             fake_ans = Discriminator(fake_inputs, training=True)
                             real_ans = Discriminator(real_inputs, training=True)
-                            # tf.print(fake_ans, real_ans)
-                            # fake_ans = tf.where(fake_ans < 0.5, 0., 1.)
-                            # real_ans = tf.where(real_ans < 0.5, 0., 1.)
-                            # tf.print(fake_ans, real_ans)
-                            tf.print("real", tf.reduce_sum(real_ans))
-                            tf.print("fake", tf.reduce_sum(fake_ans))
-                            # tf.print(d_para)
+                            # tf.print("real", tf.reduce_sum(real_ans))
+                            # tf.print("fake", tf.reduce_sum(fake_ans))
                             d_loss = get_d_loss(real_ans, fake_ans)     #get the loss of discriminator
                         d_grad = d_tape.gradient(d_loss, d_para)    #calculate the gradient
-                        tf.print("------updating discriminator------")
-                        d_optim.apply_gradients(zip(d_grad, d_para))    #update discriminator parameters
+                        # tf.print("------updating discriminator------")
+                        # tf.print("pre_tar", pre_d_loss, "tar", d_loss)
+                        d_optim.apply_gradients(zip(d_grad, d_para))
+                        if (tf.abs(pre_d_loss - d_loss) < 0.0001):
+                            break
+                            # continue
+                        pre_d_loss = d_loss    #update discriminator parameters
                     g_loss = get_g_loss(fake_ans)    #get the loss of generator
                 g_grad = g_tape.gradient(g_loss, g_para)    #calculate the gradient
                 tf.print("------updating generator------")
+                tf.print("fake", tf.reduce_sum(fake_ans))
+                tf.print("g_loss", g_loss)
                 g_optim.apply_gradients(zip(g_grad, g_para))    #update generator parameters
+                g_metric(args.n_dim, 0.01, 1000)
 
             loss, reg = get_kr_loss(x_t)    #get the loss of krnet
-        # tf.print("------updating krnet------")
+        tf.print("------updating krnet------")
+        tf.print("kr_loss", loss)
         grads = kr_tape.gradient(loss, kr_para) #calculate the gradient
 
-        # kr_optim.apply_gradients(zip(grads, kr_para))   #update krnet parameters
-        return loss, g_loss, d_loss, reg
+        kr_optim.apply_gradients(zip(grads, kr_para))   #update krnet parameters
+        return loss, g_loss, d_loss
 
-    # @tf.function
-    # def gan_train_step(real_inputs, g_para, d_para):
-    #     tf.print("------gan_step_starts------")
-    #     z = BR_data.gen_nd_Gaussian(args.n_dim, real_inputs.shape[0])
-    #     with tf.GradientTape() as g_tape:
-    #         fake_inputs = Generator(z, training=True)
-    #         fake_inputs = inv_sphere_proj(fake_inputs, args.n_dim, fake_inputs.shape[0], args.radius)
-    #         sample_gaussian_vector = BR_data.gen_nd_Gaussian(args.n_dim + 1, fake_inputs.shape[0])
-    #         fake_inputs = fake_inputs + sample_gaussian_vector
-    #
-    #         for i in range(args.d_epoch):
-    #             with tf.GradientTape() as d_tape:
-    #                 tf.print("------dis_step%s------"%(i + 1))
-    #                 fake_ans = Discriminator(fake_inputs, training=True)
-    #                 real_ans = Discriminator(real_inputs, training=True)
-    #                 d_loss = get_d_loss(real_ans, fake_ans)
-    #                 d_grad = d_tape.gradient(d_loss, d_para)
-    #                 tf.print("------updating discriminator------")
-    #                 # tf.print(d_grad)
-    #                 d_optim.apply_gradients(zip(d_grad, d_para))
-    #
-    #         g_loss = get_g_loss(fake_inputs)
-    #         g_grad = g_tape.gradient(g_loss, g_para)
-    #         tf.print("------updating generator------")
-    #         g_optim.apply_gradients(zip(g_grad, g_para))
-    #     return g_loss, d_loss
-
-    # def D_step(inputs, kr_para, g_para, d_para):
-    #     x_t = inputs
-    #     d_loss, g_loss = 0.0, 0.0
-    #     with tf.GradientTape() as kr_tape:
-    #         """generate real data"""
-    #         y_t = kr_model(x_t)[0]
-    #         y_t = inv_sphere_proj(y_t, args.n_dim, x_t.shape[0], args.radius)  # invert sphere projection
-    #         sample_gaussian_vector = BR_data.gen_nd_Gaussian(args.n_dim + 1, y_t.shape[0])  # generate stochastic noise
-    #         y_t = y_t + sample_gaussian_vector
-    #
-    #         """generator step"""
-    #         for i in tf.range(args.g_epoch):
-    #             # tf.print("------gan_step", i + 1, "------")
-    #             real_inputs = y_t
-    #             z = BR_data.gen_nd_Gaussian(args.n_dim, real_inputs.shape[0])  # stochastic input
-    #             with tf.GradientTape() as g_tape:
-    #                 """generate fake data"""
-    #                 fake_inputs = Generator(z, training=True)  # generate fake data
-    #                 fake_inputs = inv_sphere_proj(fake_inputs, args.n_dim, fake_inputs.shape[0],
-    #                                               args.radius)  # invert sphere projection
-    #                 sample_gaussian_vector = BR_data.gen_nd_Gaussian(args.n_dim + 1,
-    #                                                                  fake_inputs.shape[0])  # generate stochastic noise
-    #                 fake_inputs = fake_inputs + sample_gaussian_vector
-    #                 fake_ans = Discriminator(fake_inputs, training=True)
-    #                 # fake_ans = tf.where(fake_ans < 0.5, x=0., y=1.)
-    #                 # fake_ans = getBinaryTensor(fake_ans)
-    #                 for j in tf.range(args.d_epoch):
-    #                     with tf.GradientTape() as d_tape:
-    #                         """discriminator step"""
-    #                         # tf.print("------dis_step", j + 1, "------")
-    #                         real_ans = Discriminator(real_inputs, training=True)
-    #                         tf.print("real", tf.reduce_sum(real_ans))
-    #                         tf.print("fake", tf.reduce_sum(fake_ans))
-    #                         d_loss = get_d_loss(real_ans, fake_ans)  # get the loss of discriminator
-    #                     d_grad = d_tape.gradient(d_loss, d_para)  # calculate the gradient
-    #                     tf.print(d_grad)
-    #                     tf.print("------updating discriminator------")
-    #                     tf.print("d_loss", d_loss)
-    #                     d_optim.apply_gradients(zip(d_grad, d_para))  # update discriminator parameters
-    #
-    #
-    #                 g_loss = get_g_loss(fake_ans)  # get the loss of generator
-    #                 tf.print("                              g_loss", g_loss)
-    #             g_grad = g_tape.gradient(g_loss, g_para)  # calculate the gradient
-    #             tf.print("------updating generator------")
-    #             # g_optim.apply_gradients(zip(g_grad, g_para))  # update generator parameters
-    #
-    #         loss, reg = get_kr_loss(x_t)  # get the loss of krnet
-    #     # tf.print("------updating krnet------")
-    #     grads = kr_tape.gradient(loss, kr_para)  # calculate the gradient
-    #     # tf.print("*********************************kr_loss", loss)
-    #     # kr_optim.apply_gradients(zip(grads, kr_para))  # update krnet parameters
-    #     return loss, g_loss, d_loss, reg
 
     n_epochs = args.n_epochs
-    with summary_writer.as_default():
-        # iterate over epochs
-        print("---------------start-training-----------------")
-        for i in tf.range(1,n_epochs+1):
-            # iterate over the batches of the dataset
-            for step, train_batch in enumerate(train_dataset):
-                tf.print("step", step + 1)
-                loss, g_l, d_l, reg = kr_train_step(train_batch, kr_para, g_para, d_para)
-                # loss, g_l, d_l, reg = D_step(train_batch, kr_para, g_para, d_para)
-                tf.print("kr_loss =", loss,"g_l =", g_l, "d_l =", d_l, "reg =", reg)
+    # iterate over epochs
+    print("---------------start-training-----------------")
+    for i in tf.range(1,n_epochs+1):
+        # iterate over the batches of the dataset
+        for step, train_batch in enumerate(train_dataset):
+            tf.print("step", step + 1)
+            loss, g_l, d_l = kr_train_step(train_batch, kr_para, g_para, d_para)
+            # loss, g_l, d_l, reg = D_step(train_batch, kr_para, g_para, d_para)
+            tf.print("kr_loss =", loss,"g_l =", g_l, "d_l =", d_l)
 
         # for step, train_batch in enumerate(train_dataset):
         #     tf.print("step", step + 1)
@@ -352,8 +301,8 @@ if __name__ == '__main__':
     p.add_argument('--n_width', type=int, default=32, help='The number of neurons for the hidden layers.')
     p.add_argument('--n_step', type=int, default=1, help='The step size for dimension reduction in each squeezing layer.')
     p.add_argument('--rotation', action='store_true', help='Specify rotation layers or not?')
-    p.add_argument('--d_epoch', type=int, default=50, help='The number discriminator updates during one epoch of generator')
-    p.add_argument('--g_epoch', type=int, default=100, help='The number generator updates during one epoch of krnet')
+    p.add_argument('--d_epoch', type=int, default=500, help='The number discriminator updates during one epoch of generator')
+    p.add_argument('--g_epoch', type=int, default=10, help='The number generator updates during one epoch of krnet')
     #p.set_defaults(rotation=True)
     p.add_argument('--n_bins4cdf', type=int, default=0, help='The number of bins for uniform partition of the support of PDF.')
     p.add_argument('--flow_coupling', type=int, default=1, help='Coupling type: 0=additive, 1=affine.')
@@ -365,9 +314,9 @@ if __name__ == '__main__':
     p.add_argument("--n_train", type=int, default=100, help='The number of samples in the training set.')
     p.add_argument('--batch_size', type=int, default=50, help='Batch size of training generator.')
     p.add_argument("--kr_lr", type=float, default=0.001, help='Base krnet learning rate.')
-    p.add_argument("--g_lr", type=float, default=0.001, help='Base generator learning rate.')
+    p.add_argument("--g_lr", type=float, default=0.0001, help='Base generator learning rate.')
     p.add_argument("--d_lr", type=float, default=0.001, help='Base discriminator learning rate.')
-    p.add_argument('--n_epochs',type=int, default=1, help='Total number of training epochs.')
+    p.add_argument('--n_epochs',type=int, default=10, help='Total number of training epochs.')
 
     # samples:
     p.add_argument("--n_samples", type=int, default=10000, help='Sample size for the trained model.')
